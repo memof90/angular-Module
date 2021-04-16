711c3528ac5a9e2081ad70e369a9b86b39c7103c
Memo <memof80@gmail.com>
2021-04-16 15:55:20 -0500

    FormsModule

--- src/app/avenger/avenger.module.ts
+++ src/app/avenger/avenger.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MainPageComponent } from './main-page/main-page.component';
+import { FormsModule } from '@angular/forms';
 
+import { MainPageComponent } from './main-page/main-page.component';
 
 
 @NgModule({
   declarations: [MainPageComponent],
   exports: [MainPageComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule
   ]
 })
 export class AvengerModule { }
