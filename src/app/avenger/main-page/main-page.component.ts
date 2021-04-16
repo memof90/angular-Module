3c45230c758b6289ca5a4c7845b3c9b7ab35d8f2
Memo <memof80@gmail.com>
2021-04-16 16:16:45 -0500

    two way data binding -> emite y recibe eventos o propiedades ngModel to preventd value of forms in angular

--- src/app/avenger/main-page/main-page.component.ts
+++ src/app/avenger/main-page/main-page.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
 
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html'
 })
 export class MainPageComponent {
 
+  nuevo: Personaje = {
+    nombre: 'Iroman',
+    poder: 14000
+  };
+// two way data binding -> emite y recibe eventos o propiedades
+  // cambiarNombre(event: any): void {
+  //   event.preventDefault();
+  // }
   add(): void  {
-  console.log('hey');
+  console.log(this.nuevo);
   }
 
 }
