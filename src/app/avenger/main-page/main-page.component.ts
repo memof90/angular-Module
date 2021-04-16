27c3fe081faf13a44c14c7082eb24e6aacc18900
Memo <memof80@gmail.com>
2021-04-16 17:10:01 -0500

    Mostrar listado de componentes ngFor

--- src/app/avenger/main-page/main-page.component.ts
+++ src/app/avenger/main-page/main-page.component.ts
@@ -11,16 +11,35 @@ interface Personaje {
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Iroman',
+      poder: 15000
+    },
+    {
+      nombre: 'Capitan America',
+      poder: 7500
+    },
+  ];
+
   nuevo: Personaje = {
-    nombre: 'Iroman',
-    poder: 14000
+    nombre: '',
+    poder: 0
   };
 // two way data binding -> emite y recibe eventos o propiedades
   // cambiarNombre(event: any): void {
   //   event.preventDefault();
   // }
   add(): void  {
-  console.log(this.nuevo);
-  }
+    if (this.nuevo.nombre.trim().length === 0) {
+      return;
+    }
+    console.log(this.nuevo);
+    this.personajes.push(this.nuevo);
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+  };
 
+}
 }
